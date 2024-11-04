3c9b8fa6d5efe8410a454529a9371c2ed1265653
Josszzz <josselin.duchateau@gmail.com>
2024-11-04 23:35:56 +0100

    Update, use ODM

--- app/auth.py
+++ app/auth.py
@@ -1,17 +1,12 @@
 from fastapi.security import OAuth2AuthorizationCodeBearer
 from keycloak import KeycloakOpenID
 from settings import *
-from fastapi import Security, HTTPException, status,Depends
+from fastapi import Security, HTTPException, status, Depends
 from schemas import User
-from aiohttp import ClientSession
-import logging
-logger = logging.getLogger('uvicorn.error')
-
-# This is used for fastapi docs authentification
+from jwcrypto import jwk, jws
+from functools import lru_cache
 
 
-# This actually does the auth checks
-# client_secret_key is not mandatory if the client is public on keycloak
 keycloak_openid = KeycloakOpenID(
     server_url=KEYCLOAK_INTERNAL_SERVER_URL,
     client_id=KEYCLOAK_CLIENT_ID,
@@ -24,17 +19,40 @@ oauth2_scheme = OAuth2AuthorizationCodeBearer(
     tokenUrl=KEYCLOAK_TOKEN_URL
 )
 
+
+@lru_cache
+def get_public_key() -> str:
+    # Get the public key from the server
+    key = (
+        "-----BEGIN PUBLIC KEY-----\n"
+        + keycloak_openid.public_key()
+        + "\n-----END PUBLIC KEY-----"
+    )
+    
+    key = jwk.JWK.from_pem(key.encode("utf-8"))
+    return key
+
 # Get the payload/token from keycloak
 async def get_payload(token: str = Security(oauth2_scheme)) -> dict:
+    key = get_public_key()
     try:
-        return keycloak_openid.decode_token(
-            token
-        )
+        return keycloak_openid.decode_token(token, key=key, validate=True)
+    except jws.InvalidJWSSignature: 
+        # Reset the public key cache
+        get_public_key.cache_clear()
+        key = get_public_key()
+        try:
+            return keycloak_openid.decode_token(token, key=key, validate=True)
+        except Exception as e:
+            raise HTTPException(
+                status_code=status.HTTP_401_UNAUTHORIZED,
+                detail= f'Peristent error after public key refresh: {e}',
+                headers={"WWW-Authenticate": "Bearer"},
+            )
     except Exception as e:
-        logger.error
         raise HTTPException(
             status_code=status.HTTP_401_UNAUTHORIZED,
-            detail=str(e), # "Invalid authentication credentials",
+            detail = str(e), 
             headers={"WWW-Authenticate": "Bearer"},
         )
     
@@ -53,6 +71,18 @@ async def get_user_info(payload: dict = Depends(get_payload)) -> User:
     except Exception as e:
         raise HTTPException(
             status_code=status.HTTP_400_BAD_REQUEST,
-            detail=str(e), # "Invalid authentication credentials",
+            detail=str(e), 
             headers={"WWW-Authenticate": "Bearer"},
         )
+
+def check_role(role: str):
+    async def _check_role(user: User = Depends(get_user_info)):
+        if role in user.realm_roles:
+            return
+        
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail='User does not have the appropriate roles', 
+            headers={"WWW-Authenticate": "Bearer"},
+        )
+    return _check_role
