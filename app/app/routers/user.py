546ac2e4a41e98595561bda83f4d5ba4ee52a8bf
BSac33 <155906069+BSac33@users.noreply.github.com>
2024-11-17 10:56:48 +0100

    Add files via upload
    Added label structure as .json with initialization at mongodb startup
    Modified data structure in db.py to suit frontend 
    Modified routes upload_episode and upload_egm
    Implemented role mapping for AI models and listing of compatible AI models for each episode_type (Boston only)
    Added Keycloak interaction in services folder
    Added label retrieval from database in services folder

---
+++ app/app/routers/user.py
@@ -0,0 +1,29 @@
+from fastapi import APIRouter, Depends
+from typing import Annotated
+from schemas import User
+from fastapi import Form
+from auth import get_user_info, get_token_with_credentials
+
+
+router = APIRouter(
+    tags=["user"]
+)
+
+@router.get("/roles")
+def user_roles(user: Annotated[User, Depends(get_user_info)]):
+    return {'realm_roles': user.realm_roles,
+            'client_roles': user.client_roles}
+    
+# Exemple d'utilisation dans une route
+@router.post("/login")
+async def login(
+    username: Annotated[str, Form()], 
+    password: Annotated[str, Form()]
+    ):
+    token = await get_token_with_credentials(username, password)
+    return {
+        "access_token": token["access_token"],
+        "token_type": "bearer",
+        "refresh_token": token["refresh_token"]
+    }
+
